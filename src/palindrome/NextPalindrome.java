b5edd43eb0f1106172812f8535f7314ca07d7c6d
Lutz, Bryan <LUTZB1@nationwide.com>
2018-02-15 11:59:59 -0500

    Initial Project Commit

---
+++ src/palindrome/NextPalindrome.java
@@ -0,0 +1,10 @@
+package palindrome;
+
+public class NextPalindrome {
+
+	public static void main(String[] args) {
+		// TODO: For a given positive integer K of not more than 1000000 digits, 
+		// write the value of the smallest palindrome larger than K to output
+	}
+
+}
