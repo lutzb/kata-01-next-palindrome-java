ac97c4e0624bf8bedbeb1d31dc1efcff1620cfe8
Lutz, Bryan <LUTZB1@nationwide.com>
2018-02-15 12:30:57 -0500

    Add red tests.

--- src/palindrome/NextPalindrome.java
+++ src/palindrome/NextPalindrome.java
@@ -2,9 +2,11 @@ package palindrome;
 
 public class NextPalindrome {
 
-	public static void main(String[] args) {
+	public static int getNextPalindrome(int input) {
 		// TODO: For a given positive integer K of not more than 1000000 digits, 
 		// write the value of the smallest palindrome larger than K to output
+		
+		return 0;
 	}
 
 }
--- src/test/NextPalindromeText.java
+++ src/test/NextPalindromeText.java
@@ -4,11 +4,42 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import palindrome.NextPalindrome;
+
 class NextPalindromeText {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testNextPalindromeWithOneDigit() {
+		assertEquals(2, NextPalindrome.getNextPalindrome(1));
+	}
+	
+	@Test
+	void testNextPalindromeWithTwoDigits() {
+		assertEquals(33, NextPalindrome.getNextPalindrome(31));
+	}
+	
+	@Test
+	void testNextPalindromeWithThreeDigits() {
+		assertEquals(525, NextPalindrome.getNextPalindrome(519));
 	}
 
+	@Test
+	void testNextPalindromeWithFourDigits() {
+		assertEquals(7337, NextPalindrome.getNextPalindrome(7236));
+	}
+	
+	@Test
+	void testNextPalindromeWithFiveDigits() {
+		assertEquals(23632, NextPalindrome.getNextPalindrome(23608));
+	}
+	
+	@Test
+	void testNextPalindromeWithDigitRollover() {
+		assertEquals(101, NextPalindrome.getNextPalindrome(99));
+	}
+	
+	@Test
+	void testNextPalindromeWithPalindrome() {
+		assertEquals(111, NextPalindrome.getNextPalindrome(101));
+	}
 }
